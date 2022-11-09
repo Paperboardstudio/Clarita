b9fd781ca804cb88633a3a4246c777a7f2e2e218
miguelfrnandez <miguelfrnandez@gmail.com>
2022-11-09 00:55:59 -0500

    Feat: Chef Registration layout input fields
    Being develop

--- Android/app/src/main/java/com/example/clarita/ChefRegistration.java
+++ Android/app/src/main/java/com/example/clarita/ChefRegistration.java
@@ -9,6 +9,6 @@ public class ChefRegistration extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_chef_registration2);
+        setContentView(R.layout.activity_chef_registration);
     }
 }
