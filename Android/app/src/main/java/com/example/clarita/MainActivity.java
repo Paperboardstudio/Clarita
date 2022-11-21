ebf0491d8f0641b57df90e14498f8b4f889cda10
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2022-11-21 17:01:44 +0200

    RCA-12  STEP 2: Splash Screen & Login/Signup Layout || Java || Android Studio || Firebase
    Added home page
    Added buttons for all home page functions
    Created background colours

--- Android/app/src/main/java/com/example/clarita/MainActivity.java
+++ Android/app/src/main/java/com/example/clarita/MainActivity.java
@@ -1,56 +1,56 @@
 package com.example.clarita;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 
-import android.view.View;
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.content.Intent;
+import android.media.Image;
+import android.os.Bundle;
+import android.os.Handler;
+import android.widget.ImageView;
+import android.widget.TextView;
 
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+    ImageView imageView;
+    TextView textView;
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-    }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+            imageView = (ImageView) findViewById(R.id.imageView);
+            textView = (TextView)  findViewById(R.id.textView7);
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
+            imageView.animate().alpha(0f).setDuration(0);
+            textView.animate().alpha(0f).setDuration(0);
+
+
+            imageView.animate().alpha(1f).setDuration(1000).setListener(new AnimatorListenerAdapter() {
+                /**
+                 * {@inheritDoc}
+                 *
+                 * @param animation
+                 */
+
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    textView.animate().alpha(1f).setDuration(800);
+                }
+            });
+            new Handler().postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent = new Intent(MainActivity.this,MainMenu.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },3000);
         }
 
-        return super.onOptionsItemSelected(item);
-    }
+
 }
