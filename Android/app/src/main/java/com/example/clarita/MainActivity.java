3c255b77263933aaa8651f7dd18f80ba446411ce
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2022-12-27 21:39:25 +0200

    RCA-22  STEP 12: Delivery Registration,Verification & Login|| Firebase||Android Studio||Java

--- Android/app/src/main/java/com/example/clarita/MainActivity.java
+++ Android/app/src/main/java/com/example/clarita/MainActivity.java
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        imageView = (ImageView) findViewById(R.id.imageView);
-        textView = (TextView) findViewById(R.id.textView7);
+        imageView = (ImageView)findViewById(R.id.imageView);
+        textView = (TextView)findViewById(R.id.textView7);
 
         imageView.animate().alpha(0f).setDuration(0);
         textView.animate().alpha(0f).setDuration(0);
@@ -59,34 +59,39 @@ public class MainActivity extends AppCompatActivity {
             public void run() {
 
                 Fauth = FirebaseAuth.getInstance();
-                if (Fauth.getCurrentUser() != null) {
-                    if (Fauth.getCurrentUser().isEmailVerified()) {
-                        Fauth = FirebaseAuth.getInstance();
+                if(Fauth.getCurrentUser()!=null){
+                    if(Fauth.getCurrentUser().isEmailVerified()){
+                        Fauth=FirebaseAuth.getInstance();
 
-                        databaseReference = FirebaseDatabase.getInstance().getReference("User").child(FirebaseAuth.getInstance().getUid() + "/Role");
+                        databaseReference = FirebaseDatabase.getInstance().getReference("User").child(FirebaseAuth.getInstance().getUid()+"/Role");
                         databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
                             @Override
                             public void onDataChange(@NonNull DataSnapshot snapshot) {
                                 String role = snapshot.getValue(String.class);
 
-                                if (role.equals("Chef")) {
-                                    startActivity(new Intent(MainActivity.this, ChefFoodPanel_BottomNavigation.class));
+                                if(role.equals("Chef")){
+                                    startActivity(new Intent(MainActivity.this,ChefFoodPanel_BottomNavigation.class));
                                     finish();
                                 }
 
-                                if (role.equals("Customer")) {
-                                    startActivity(new Intent(MainActivity.this, CustomerFoofPanel_BottomNavigation.class));
+                                if(role.equals("Customer")){
+                                    startActivity(new Intent(MainActivity.this,CustomerFoofPanel_BottomNavigation.class));
+                                    finish();
+                                }
+
+                                if(role.equals("DeliveryPerson")) {
+                                    startActivity(new Intent(MainActivity.this, DeliveryFoodPanel_BottomNavigation.class));
                                     finish();
                                 }
                             }
 
                             @Override
                             public void onCancelled(@NonNull DatabaseError error) {
-                                Toast.makeText(MainActivity.this, error.getMessage(), Toast.LENGTH_LONG).show();
+                                Toast.makeText(MainActivity.this,error.getMessage(),Toast.LENGTH_LONG).show();
                             }
                         });
                     }
-                    else {
+                    else{
                         AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
                         builder.setMessage("Check Whether You Have Verified Your Detail , Otherwise Please Verify");
                         builder.setCancelable(false);
@@ -94,7 +99,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
                                 dialog.dismiss();
-                                Intent intent = new Intent(MainActivity.this, MainMenu.class);
+                                Intent intent = new Intent(MainActivity.this,MainMenu.class);
                                 startActivity(intent);
                                 finish();
                             }
@@ -104,13 +109,12 @@ public class MainActivity extends AppCompatActivity {
                         Fauth.signOut();
                     }
                 }
-
                 else {
                     Intent intent = new Intent(MainActivity.this, MainMenu.class);
                     startActivity(intent);
                     finish();
                 }
             }
-        }, 3000);
+        },3000);
     }
 }
