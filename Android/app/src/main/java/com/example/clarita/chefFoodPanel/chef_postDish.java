6ecde09177b5b0b3db1b1919497525775f11abc1
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2022-12-29 23:01:32 +0200

    RCA-24 STEP 14: Firebase Sign Out & Chef Post Dish || Firebase || Android Studio || Java

---
+++ Android/app/src/main/java/com/example/clarita/chefFoodPanel/chef_postDish.java
@@ -0,0 +1,16 @@
+package com.example.clarita.chefFoodPanel;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.clarita.R;
+
+public class chef_postDish extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chef_post_dish);
+    }
+}
