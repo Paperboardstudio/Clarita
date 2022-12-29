e0d0501c40395b72866d40c331661666240dadee
Stefanos Edvard Kapsoritakis <stefan7@hotmail.gr>
2022-12-29 22:26:30 +0200

    RCA-23 STEP 13: Bottom Navigation Bar From Scratch With Fragments||Android Studio||Firebase||Java

---
+++ Android/app/src/main/java/com/example/clarita/chefFoodPanel/ChefHomeFragment.java
@@ -0,0 +1,23 @@
+package com.example.clarita.chefFoodPanel;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.clarita.R;
+
+public class ChefHomeFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_chef_home, null);
+        getActivity().setTitle("Home");
+        return v;
+    }
+}
---
+++ Android/app/src/main/java/com/example/clarita/chefFoodPanel/ChefOrderFragment.java
@@ -0,0 +1,23 @@
+package com.example.clarita.chefFoodPanel;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.clarita.R;
+
+public class ChefOrderFragment extends Fragment{
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_chef_orders,null);
+        getActivity().setTitle("New Orders");
+        return v;
+    }
+}
